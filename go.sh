c0bdd25be4e5973e58697a998ca0b6fd0fc2e02a
Oliver Day <oliver.day@gmail.com>
2025-04-18 22:38:29 +0000

    Auto generate targets for the dns resolver.

--- go.sh
+++ go.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# DNS resolver commands for detected patterns
+# DNS resolver commands for detected patterns - running in parallel
 
 # Pattern: sunpass.com-XXXX.win
-./target/release/rust-dns-resolver --pattern 'sunpass\.com-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp"
+./target/release/rust-dns-resolver --pattern 'sunpass\.com-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
 
 # Pattern: txtag.org-XXX.win
-./target/release/rust-dns-resolver --pattern 'txtag\.org-[a-z]{3}\.win' -c 100 -H 50 --status-path "/front/checkIp"
+./target/release/rust-dns-resolver --pattern 'txtag\.org-[a-z]{3}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
 
 # Pattern: txtag.org-XXXX.win
-./target/release/rust-dns-resolver --pattern 'txtag\.org-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" 
+./target/release/rust-dns-resolver --pattern 'txtag\.org-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Pattern: mass.gov-XXXX.win
+./target/release/rust-dns-resolver --pattern 'mass\.gov-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Pattern: michigan.gov-eXXXXX.win
+./target/release/rust-dns-resolver --pattern 'michigan\.gov-e[a-z]{5}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Pattern: ncquickpass.com-XXXX.win
+./target/release/rust-dns-resolver --pattern 'ncquickpass\.com-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Pattern: ohioturnpike.org-XXXX.win
+./target/release/rust-dns-resolver --pattern 'ohioturnpike\.org-[a-z]{4}\.win' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Pattern: paturnpike.com-XXX.cc
+./target/release/rust-dns-resolver --pattern 'paturnpike\.com-[a-z]{3}\.cc' -c 100 -H 50 --status-path "/front/checkIp" &
+
+# Wait for all background processes to complete
+wait 
